ccd5a74518682f9ff00bbe0172e3d7d553a85513
Matheus Castro <49662686+Couks@users.noreply.github.com>
2020-09-06 23:39:02 -0300

    fist commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:the_basics/Views/home/home_view.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'First Flutter App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: HomeView());
+  }
+}
